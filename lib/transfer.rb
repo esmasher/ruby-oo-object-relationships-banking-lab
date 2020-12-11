e34b5451036f6ec984ccfb02a61792b5ea9ccb13
esmasher <esmasherr@gmail.com>
2020-12-11 17:00:05 -0600

    commit tree fb6ae2eb44de24f0c1723736e8b7a40315060d22

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
+require "pry"
 class Transfer
-  # your code here
+
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize (sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+ def valid?
+  self.sender.balance >= self.amount
+  self.receiver.balance >= self.amount
+  self.sender.valid? && self.receiver.valid?
+ end
+
+def execute_transaction
+
+  if self.status == "pending" && self.sender.valid? && self.receiver.valid? && self.valid?
+binding.pry
+    @receiver.balance += @amount
+    @sender.balance -= @amount
+
+    self.status = "complete"
+  else
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+
+  end
+end
+
 end
